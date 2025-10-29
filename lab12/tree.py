11782661e7c3419631f786eb9eb7f1afc0347284
Alexis <ramirezalexis1029@gmail.com>
2025-10-29 14:06:22 -0700

    Played around with lab12

--- lab12/tree.py
+++ lab12/tree.py
@@ -2,16 +2,16 @@ from math import radians
 from Tree.core import Tree
 from PIL import Image
 
-background_color = (200, 240, 250)
-leaf_color = (30, 160, 30)
-trunk_width = 30
+background_color = (210, 255, 255)
+leaf_color = (158, 158, 158)
+trunk_width = 150
 base_trunk_color = (50, 40, 0)
-small_branch_color = (180, 130, 30)
+small_branch_color = (110, 50, 0)
 branch_gradient = (*base_trunk_color, *small_branch_color)
 
-trunk_length = 200
+trunk_length = 1000
 first_branch_line = (0, 0, 0, -trunk_length)
-scales_and_angles = [(0.75, radians(-30)), (0.75, radians(30))]
+scales_and_angles = [(0.9, radians(-15)), (0.9, radians(15))]
 age = 10
 
 tree = Tree(pos=first_branch_line, branches=scales_and_angles)
